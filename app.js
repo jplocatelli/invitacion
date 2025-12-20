44021828bfb6f18ba70721a784dd03b0702e6baa
Juan Pablo Locatelli <jplocatelli@publica.la>
2025-12-20 16:30:28 -0300

    Updated animations

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-AOS.init({
-    duration: 1000,
-    mirror: true,
-});
+const {animate, createTimer, createTimeline , utils, onScroll} = anime;
+const debug = true;
 
 // Script de confirmación dinámico
 const params = new URLSearchParams(window.location.search);
@@ -16,32 +14,48 @@ if (links[invitado]) {
     document.getElementById("confirm").href = links[invitado];
 }
 
-document.addEventListener('aos:in:primary-text', ({ detail }) => {
-  console.log('animated in', detail);
+// Wrap every letter in a span
+var textWrapper = document.querySelectorAll('.primary-text');
+textWrapper.forEach(text => {
+  text.innerHTML = text.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 });
 
-document.addEventListener('aos:out:primary-text', ({ detail }) => {
-  console.log('animated out', detail);
-});
+const primaryTextAnimation = {
+  x: ['-14px', 0],
+  filter: ['blur(16px)', 'blur(0px)'],
+  duration: 1000,
+  delay: (el, i) => Math.random() * 500
+};
 
-// // Wrap every letter in a span
-// var textWrapper = document.querySelector('.aos-init.aos-animate[data-aos="primary-text"]');
-// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+const onScrollSettings = {
+  container: '.scroller',
+  repeat: true,
+  sync: 'play reset',
+  debug 
+}
 
-// anime.timeline({loop: false})
+for (let index = 1; index < 5; index++) {
+  animate(`#section${index} .primary-text .letter`, {
+    ...primaryTextAnimation,
+    autoplay: onScroll({...onScrollSettings, target: `#section${index} .primary-text`}),
+  });
+}
+
+// timeline({loop: false})
 //   .add({
-//     targets: '.aos-init.aos-animate[data-aos="primary-text"] .letter',
-//     translateX: [40,0],
+//     targets: '.primary-text .letter',
+//     filter: ['blur(16px)', 'blur(0px)'],
+//     translateX: [-40,0],
 //     translateZ: 0,
-//     opacity: [0,1],
 //     easing: "easeOutExpo",
-//     duration: 1200,
-//     delay: (el, i) => 500 + 30 * i
-//   }).add({
-//     targets: '.aos-init.aos-animate[data-aos="primary-text"] .letter',
-//     translateX: [0,-30],
-//     opacity: [1,0],
-//     easing: "easeInExpo",
-//     duration: 1100,
-//     delay: (el, i) => 100 + 30 * i
-//   });
+//     duration: 2000,
+//     delay: (el, i) => Math.random() * 1000
+//   })
+//   // .add({
+//   //   targets: '.primary-text .letter',
+//   //   translateX: [0,-30],
+//   //   opacity: [1,0],
+//   //   easing: "easeInExpo",
+//   //   duration: 1100,
+//   //   delay: (el, i) => 100 + 30 * i
+//   // });
